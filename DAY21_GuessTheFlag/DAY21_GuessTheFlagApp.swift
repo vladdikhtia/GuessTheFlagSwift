c46b30b3749aed645f81559ebc3383f7e3d7556e
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-02-01 18:02:21 +0100

    Initial Commit

---
+++ DAY21_GuessTheFlag/DAY21_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAY21_GuessTheFlagApp.swift
+//  DAY21_GuessTheFlag
+//
+//  Created by Vladyslav Dikhtiaruk on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct DAY21_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
